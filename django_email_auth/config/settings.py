585d7d1f4b844a86aaed819ca7d1b46b128f5238
etcilopp <kirikeza@gmail.com>
2025-07-21 11:42:11 -0600

    updated settings

--- django_email_auth/config/settings.py
+++ django_email_auth/config/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = "django_email_auth.config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "django_email_auth" / "templates"],
+        "DIRS": [os.path.join(BASE_DIR, "django_email_auth", "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
